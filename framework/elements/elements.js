ab2980c67bab3f00352800a09e69a9fe76d0b28f
evos <eugene.osakovich@gmail.com>
2023-02-15 13:49:52 +0300

    [TASK#3.1] add: baseElement class is added (first implementation)

---
+++ framework/elements/elements.js
@@ -0,0 +1,30 @@
+const { BrowserFactory } = require('../browser/browser.js');
+
+
+class BaseElement{
+
+    static #driver = BrowserFactory.getInstance();
+
+    constructor(name, uniqLocator){
+        this.name = name;
+        this.uniqLocator = uniqLocator;
+    }
+
+    getElement(){
+        return BaseElement.#driver.findElement(this.uniqLocator);
+    }
+
+    isPresent(){
+        return BaseElement.#driver.elementIsDisplayed(this.uniqLocator);
+    }
+
+    click(){
+        this.isPresent();
+        return this.getElement().click();
+    }
+
+};
+
+module.exports = {
+    BaseElement
+};
